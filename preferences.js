3b8ccaca81db1ce3d827a5cd51399d9fbe3aa53a
Marwan986 <aziz.marwan@gmail.com>
2014-11-16 16:37:32 -0500

    added preference

---
+++ preferences.js
@@ -0,0 +1,50 @@
+var preferences = [
+	{
+	    
+	    "make": "GMC",
+	    "model": "1500",
+	    "kilometers": "120000-220000",
+	    "price": "5000-12000"
+	},
+	{
+	    
+	    "make": "Ford",
+	    "model": "F150",
+	    "kilometers": "130000-225000",
+	    "price": "4000-12000"
+	},
+	{
+	    
+	    "make": "Dodge",
+	    "model": "Mega cab",
+	    "kilometers": "100000-200000",
+	    "price": "7000-15000"
+	},
+	{
+	    
+	    "make": "GMC",
+	    "model": "Silverado",
+	    "kilometers": "100000-225000",
+	    "price": ""
+	},
+	{
+	    
+	    "make": "GMC",
+	    "model": "Sierra",
+	    "kilometers": "100000-225000",
+	    "price": "4000-15000"
+	},
+	{
+	    
+	    "make": "Nissan",
+	    "model": "Altima",
+	    "kilometers": "100000-225000",
+	    "price": "1000-15000"
+	},
+	{
+		"make": "Audi",
+		"model": "Q7"
+	}
+];
+
+module.exports = preferences;
