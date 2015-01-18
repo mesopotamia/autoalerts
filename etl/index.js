d650490f1555e991e02d3538fb00c60c461b7d6c
Marwan Aziz <aziz.marwan@gmail.com>
2015-01-18 18:02:59 -0500

    kijiji scrapper working well

--- etl/index.js
+++ etl/index.js
@@ -1,7 +1,7 @@
 var extract = require('./extract');
 var loader = require('./load');
 var transform = require('./transform');
-var autoTraderExtractor = require('./autotrader/transform')
+// var autoTraderExtractor = require('./autotrader/transform')
 
 //start up database for saving normalized adds, extract once started
 loader.start(function(){
@@ -10,10 +10,10 @@ loader.start(function(){
 		var collection = JSON.parse(response).results.collection1;
 		transform.start(collection);
 	});
-	extract.getData('autotrader').then(function(response){
+	/*extract.getData('autotrader').then(function(response){
 		var results = JSON.parse(response).results.collection1;
 		autoTraderExtractor.start(results);
-	})
+	})*/
 
 });
 
